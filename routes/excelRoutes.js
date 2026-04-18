f67e9a04f003b9ac185ab132f18562a2a2924b28
Saqlain-313 <saqlainmalik@maxifysolution.com>
2026-04-18 18:10:56 +0530

    by malik

--- routes/excelRoutes.js
+++ routes/excelRoutes.js
@@ -2,12 +2,17 @@
 
 import express from "express";
 import upload from "../middleware/upload.js";
-import { uploadExcel } from "../controllers/excelController.js";
+import { getMyImportedUsers, getSingleImportedUser, uploadExcel } from "../controllers/excelController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 // 🔒 Protected route (optional)
 router.post("/upload-excel", protect, upload.single("file"), uploadExcel);
+
+router.get("/my-users", protect, getMyImportedUsers);
+
+// 🔍 Get single user by ID
+router.get("/user/:id", protect, getSingleImportedUser);
 
 export default router;
